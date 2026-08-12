8c0a23d3349e4288f261f0a7032577ca0d8057da
temma_h <temma.uesugi@gmail.com>
2026-08-12 10:58:35 +0900

    update: ブロックスコープの必要あり

--- MasterData/Entities/MasterSample.cs
+++ MasterData/Entities/MasterSample.cs
@@ -1,18 +1,21 @@
 using MasterMemory;
 using MessagePack;
 
-namespace MasterData.Entities;
-
-/// <summary>
-/// マスタのサンプル
-/// </summary>
-[MemoryTable("MasterSample"), MessagePackObject(true)]
-public class MasterSample(int id, string name)
+//Note:ブロックスコープの必要がある
+namespace MasterData.Entities
 {
-    /// <summary>ID</summary>
-    [PrimaryKey]
-    public int Id { get; } = id;
+    /// <summary>
+    /// マスタのサンプル
+    /// </summary>
+    [MemoryTable("MasterSample"), MessagePackObject(true)]
+    public class MasterSample(int id, string name)
+    {
+        /// <summary>ID</summary>
+        [PrimaryKey]
+        public int Id { get; } = id;
 
-    /// <summary>名前</summary>
-    public string Name { get; } = name;
+        /// <summary>名前</summary>
+        public string Name { get; } = name;
+    }
 }
+
