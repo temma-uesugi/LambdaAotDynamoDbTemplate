0874296686e06cda58887912eb5b7b48fae6f797
temma_h <temma.uesugi@gmail.com>
2026-08-12 10:49:41 +0900

    clean: 微修正

--- MasterData/Entities/MasterSample.cs
+++ MasterData/Entities/MasterSample.cs
@@ -1,19 +1,18 @@
 using MasterMemory;
 using MessagePack;
 
-namespace MasterData.Entities
-{
-    /// <summary>
-    /// マスタのサンプル
-    /// </summary>
-    [MemoryTable("MasterSample"), MessagePackObject(true)]
-    public class MasterSample(int id, string name)
-    {
-        /// <summary>ID</summary>
-        [PrimaryKey]
-        public int Id { get; } = id;
+namespace MasterData.Entities;
 
-        /// <summary>名前</summary>
-        public string Name { get; } = name;
-    }
+/// <summary>
+/// マスタのサンプル
+/// </summary>
+[MemoryTable("MasterSample"), MessagePackObject(true)]
+public class MasterSample(int id, string name)
+{
+    /// <summary>ID</summary>
+    [PrimaryKey]
+    public int Id { get; } = id;
+
+    /// <summary>名前</summary>
+    public string Name { get; } = name;
 }
